ea44d4c7a24bfe8b77d218fbb8a587db48776060
JohnLoftin <jgloftin@gmail.com>
2020-12-10 21:36:21 -0500

    initial commit

---
+++ src/components/landing.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import { useState } from 'react';
+
+const Landing = props => {
+    return (
+        <div>
+            <h1>Welcome To SantaTracker</h1>
+        </div>
+    )
+}
+
+export default Landing;
